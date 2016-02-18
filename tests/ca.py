261c895c458cd80636f6465fc4d41d5d168e334d
Nicola Bernardini <nicb@sme-ccppd.org>
2016-02-18 13:01:02 +0100

    corrected code for ca.py

--- tests/ca.py
+++ tests/ca.py
@@ -1,3 +1,5 @@
+import sys
+import math
 #
 # this is the Cellular Automaton transformation that is implemented for
 # Leonardo Sisto's bachelor thesis.
@@ -8,8 +10,16 @@ def ca(tfreq, tmag):
         tfreq: frequency vector, tmag: magnitude vector
 	returns y: the re-shuffled magnitudes
 	"""
-        step = tmag.size
+        step = min(tfreq.size, tmag.size)
+        print("# outer_size: %d (tfreq.size %d, tmag.size %d)" % (step, tfreq.size, tmag.size))
         k = 0
         while (k < step):
-            print("%8.4f %+9.4f\n" % (tfreq[k], tmag[k]))
+            l = 0
+            inner_size = min(tfreq[k].size, tmag[k].size)
+            print("# inner_size: %d (tfreq.size %d, tmag.size %d)" % (inner_size, tfreq[k].size, tmag[k].size))
+            while (l < inner_size):
+                sys.stdout.write("%8.4f %+9.4f " % (tfreq[k][l], tmag[k][l]))
+                l += 1
+            print("")
+            k += 1
         return tmag
